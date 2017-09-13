7d5cb9935b843b83a63f6bf397be2d19b6d29693
RevMatres <benediktheinen1@gmail.com>
2017-09-13 19:07:33 +0200

    started to learn lambdas; first put stuff in wrong file

--- 9-closures_or_lamdas.rs
+++ 9-closures_or_lamdas.rs
@@ -1,3 +1,26 @@
 fn main(){
-    // these sound fun!
+    // just a simple closure
+    let square = |i| i*i;
+    println!("square 2: {}",square(2));
+    println!("square 3: {}",square(3));
+    println!("square 25: {}",square(25));
+
+
+    // capture stuff
+
+    let mut stack: Vec<u8> = Vec::new();
+
+    // push_to_stack automatically &mut borrows stack
+    // no need to specify -> that's bloody brilliant!
+    let mut push_to_stack = |i| {
+        stack.push(i);
+        println!("the stack is now: {:?}",stack);
+    };
+
+    push_to_stack(20);
+    push_to_stack(9);
+    push_to_stack(247);
+    push_to_stack(33);
+    push_to_stack(88);
+    push_to_stack(250);
 }
