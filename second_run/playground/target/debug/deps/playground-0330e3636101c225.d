4c643171a16b7c9c44f7f3a1b605520e4c466772
RevMatres <benediktheinen1@gmail.com>
2018-06-04 23:05:15 +0200

    Added a rust playground project with Cargo. It's a playground for the second run!

---
+++ second_run/playground/target/debug/deps/playground-0330e3636101c225.d
@@ -0,0 +1,5 @@
+/home/work/Documents/projects/learning-rust/second_run/playground/target/debug/deps/playground-0330e3636101c225: src/main.rs
+
+/home/work/Documents/projects/learning-rust/second_run/playground/target/debug/deps/playground-0330e3636101c225.d: src/main.rs
+
+src/main.rs:
---
+++ second_run/playground/target/debug/playground.d
@@ -0,0 +1,1 @@
+/home/work/Documents/projects/learning-rust/second_run/playground/target/debug/playground: /home/work/Documents/projects/learning-rust/second_run/playground/src/main.rs
