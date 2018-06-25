b117c2340cb7209918988fd5961b30737dc56738
RevMatres <benediktheinen1@gmail.com>
2018-06-25 18:30:47 +0200

    made a fib function with proper error handling and modular structure

--- second_run/fib/src/main.rs
+++ second_run/fib/src/main.rs
@@ -1,3 +1,42 @@
+extern crate fib;
+
+use fib::*;
+
 fn main() {
-    println!("Hello, world!");
+
+    // Setup fib memory
+    let mut fib_memo: Vec<u32> = Vec::new();
+    fib_memo.push(0);
+    fib_memo.push(1);
+
+    // Collect commandline arguments
+    let number = get_args();
+
+    // Print the fibonacci number
+    println!("The {}th Fibonacci number is: {}", number, fib(&mut fib_memo, number));
+
+    for (index, value) in fib_memo.iter().enumerate() {
+        println!("{}    {}", index, value);
+    }
+
+}
+
+// Tests for lib.rs
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_fib_sequence() {
+
+        let mut fib_memo: Vec<u32> = Vec::new();
+        fib_memo.push(0);
+        fib_memo.push(1);
+
+        assert_eq!(fib(&mut fib_memo, 0), 0);
+        assert_eq!(fib(&mut fib_memo, 1), 1);
+        assert_eq!(fib(&mut fib_memo, 5), 5);
+
+    }
 }
