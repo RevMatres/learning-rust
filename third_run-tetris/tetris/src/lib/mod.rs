4b29ad15d031cc36113c52285cf7670c99eee3e0
RevMatres <benediktheinen1@gmail.com>
2018-11-17 22:22:54 +0100

    made a third iteration of the pseudo code, it's looking a lot like rust code now

--- third_run-tetris/tetris/src/lib/mod.rs
+++ third_run-tetris/tetris/src/lib/mod.rs
@@ -1,8 +1,38 @@
 mod game;
 mod renderer;
 
+struct Honk {
+    value: i32,
+}
+
+impl Honk {
+    fn increment(&mut self) {
+        self.value += 1;
+    }
+
+    fn runner<F>(&mut self, f: F) where
+        F: Fn(&mut Self) {
+
+            f(self);
+
+            // The clojure only captures values from the surrounding scope as reference.
+            // That doesn't affect its parameters, though (which is specified as a mutable
+            // reference to the self object)!
+        }
+}
+
 pub fn honk() {
     println!("Honk.");
     renderer::render();
     game::game();
+
+
+    let mut Tonk = Honk { value: 33 };
+    println!("Honk: {}", Tonk.value);
+
+    Tonk.increment();
+    Tonk.runner(|a| a.value += 3);
+
+    println!("Tonk: {}", Tonk.value);
+
 }
